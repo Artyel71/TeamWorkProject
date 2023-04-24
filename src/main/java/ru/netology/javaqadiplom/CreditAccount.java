8fcd739796bbc54fc3de75795b77592e2390c2e1
Alex Baskakau <lenovos6000v4@gmail.com>
2023-04-24 13:54:34 +0100

    Tests added

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -23,6 +23,7 @@ public class CreditAccount extends Account {
                     "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
+
         this.balance = initialBalance;
         this.creditLimit = creditLimit;
         this.rate = rate;
--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -1,20 +1,118 @@
 package ru.netology.javaqadiplom;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 public class CreditAccountTest {
 
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
+                1000,
+                5_000,
+                15
+        );
+
+        account.add(3000);
+
+        Assertions.assertEquals(4000, account.getBalance());
+    }
+    @Test
+    public void shouldAddIfBalanceZero() {
+        CreditAccount account = new CreditAccount(
                 0,
                 5_000,
                 15
         );
 
-        account.add(3_000);
+        account.add(3000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(3000, account.getBalance());
+    }
+    @Test
+    public void shouldAddIfBalanceNegative() {
+        CreditAccount account = new CreditAccount(
+                -1000,
+                5_000,
+                15
+        );
+
+        account.add(3000);
+
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+    @Test
+    public void shouldPayIfPositiveBalance() {
+        CreditAccount account = new CreditAccount(
+                5000,
+                5000,
+                15
+        );
+        account.pay(1000);
+        Assertions.assertEquals(4000, account.getBalance());
+    }
+    @Test
+    public void shouldPayIfBalanceZeroUnderCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+        account.pay(1000);
+        Assertions.assertEquals(-1000, account.getBalance());
+    }
+    @Test
+    public void shouldPayIfOverCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+        account.pay(6000);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+    @Test
+    public void shouldPayIfEqualCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                5000,
+                15
+        );
+        account.pay(6000);
+        Assertions.assertEquals(-5000, account.getBalance());
+    }
+    @Test
+    public void shouldThrowException(){
+        CreditAccount account = new CreditAccount(
+                -5000,
+                5000,
+                -23
+        );
+        Assertions.assertThrows(IllegalArgumentException.class,() -> account.getRate());
+    }
+    @Test
+    public void shouldCalculateYearChange() {
+        CreditAccount account = new CreditAccount(-200,
+                5000,
+                15
+        );
+        Assertions.assertEquals(-30,account.yearChange());
+    }
+
+    @Test
+    public void shouldNotCalculateYearChangeIfPositiveBalance() {
+        CreditAccount account = new CreditAccount(200,
+                5000,
+                15
+        );
+        Assertions.assertEquals(0,account.yearChange());
+    }
+    @Test
+    public void shouldCalculateYearChangeIfZeroBalance() {
+        CreditAccount account = new CreditAccount(0,
+                5000,
+                15
+        );
+        Assertions.assertEquals(0,account.yearChange());
     }
 }
